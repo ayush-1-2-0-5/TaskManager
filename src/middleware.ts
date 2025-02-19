03f321a979b29346af55509ee133523fb90e27ed
Ayush <21ucs044@lnmiit.ac.in>
2025-02-19 15:07:32 +0530

    changed middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,8 +3,6 @@ import { jwtVerify } from 'jose';
 
 export async function middleware(request: NextRequest) {
   const token = request.cookies.get('token')?.value;
-
-  // If no token, redirect to login only for protected routes
   if (!token) {
     if (['/dashboard', '/api/tasks', '/profile', '/tasks','/tasks/create','/profile/change-password', '/history'].some(route => request.nextUrl.pathname.startsWith(route))) {
       return NextResponse.redirect(new URL('/auth/login', request.url));
@@ -17,8 +15,6 @@ export async function middleware(request: NextRequest) {
       token,
       new TextEncoder().encode(process.env.JWT_SECRET)
     );
-
-    // If user is already authenticated and tries to access /auth/login, redirect to /dashboard
     if (request.nextUrl.pathname === '/auth/login') {
       return NextResponse.redirect(new URL('/dashboard', request.url));
     }
